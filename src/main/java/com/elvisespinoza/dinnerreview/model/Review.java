beda74023af68ae89a6658372be904b574b5df8b
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-02-20 14:12:12 -0500

    wip: defined the Review entity

--- src/main/java/com/elvisespinoza/dinnerreview/model/Review.java
+++ src/main/java/com/elvisespinoza/dinnerreview/model/Review.java
@@ -1,4 +1,21 @@
 package com.elvisespinoza.dinnerreview.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Review {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String submitted;
+    private Long restaurant;
+    private Integer peanutScore;
+    private Integer eggScore;
+    private Integer dairyScore;
+    private String comment;
 }
