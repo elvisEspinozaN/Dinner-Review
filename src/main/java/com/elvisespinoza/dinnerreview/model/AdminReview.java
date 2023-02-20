31fd5278021a607035e3c4d0625cac9e2962f683
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-02-20 14:24:12 -0500

    wip: data model for administrator review action

---
+++ src/main/java/com/elvisespinoza/dinnerreview/model/AdminReview.java
@@ -0,0 +1,10 @@
+package com.elvisespinoza.dinnerreview.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminReview {
+    private Boolean accept;
+}
---
+++ src/main/java/com/elvisespinoza/dinnerreview/model/ReviewStatus.java
@@ -0,0 +1,7 @@
+package com.elvisespinoza.dinnerreview.model;
+
+public enum ReviewStatus {
+    CONSIDERING,
+    RESCINDED,
+    APPROVED
+}
