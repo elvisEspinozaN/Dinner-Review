8b1cc451048393e7f7c3a701c7e3c88c26345cf3
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-02-20 23:03:53 -0500

    wip: setting data sql / h2 file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into RESTAURANT (id, name, city, state, zipcode, overall_score, egg_score) values (hibernate_sequence.nextval, 'Restaurant One', 'New York', 'NY', '12345', 5, 5);
+insert into RESTAURANT (id, name, city, state, zipcode, overall_score, peanut_score) values (hibernate_sequence.nextval, 'Restaurant Two', 'New York', 'NY', '23456', 5, 5);
+insert into RESTAURANT (id, name, city, state, zipcode, overall_score, dairy_score) values (hibernate_sequence.nextval, 'Restaurant Three', 'Los Angeles', 'CA', '78910', 5, 5);
+insert into RESTAURANT (id, name, city, state, zipcode, overall_score, peanut_score) values (hibernate_sequence.nextval, 'Restaurant Four', 'Los Angeles', 'CA', '34567', 5, 5);
